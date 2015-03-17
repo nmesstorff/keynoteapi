45452b1cc4d687c78e1ec1dc98d6558659719f34
Norman Meßtorff <normes@normes.org>
2015-03-17 18:10:24 +0100

    preparing setuptools for pypi upload

--- setup.py
+++ setup.py
@@ -8,6 +8,8 @@ import os
 from setuptools import setup, find_packages
 import keynoteapi
 
+PKG_VER = keynoteapi.__version__
+
 
 def read(*filenames, **kwargs):
     """
@@ -24,18 +26,18 @@ def read(*filenames, **kwargs):
 
 setup(
     name='keynoteapi',
+    packages=find_packages(exclude=['tests']),
     description='Access the Keynote Systems API (api.keynote.com)',
     long_description=read('README.md'),
-    version=keynoteapi.__version__,
+    version=PKG_VER,
     url='http://github.com/nmesstorff/keynoteapi/',
+    download_url="http://github.com/nmesstorff/keynoteapi/tarball/v" + PKG_VER,
     license='Apache Software License',
     author='Norman Messtorff',
     author_email='normes@normes.org',
 
     platforms='any',
-    packages=find_packages(exclude=['tests']),
     install_requires=read('requirements.txt'),
-
     setup_requires=['nose >= 1.0', 'coverage', 'pylint', 'pep8'],
     test_suite='nose.collector',
 
@@ -48,4 +50,6 @@ setup(
         'Operating System :: OS Independent',
         'Topic :: Software Development :: Libraries :: Python Modules',
         ],
+    keywords=['keynote', 'systems', 'keynote systems', 'monitoring',
+              'perfomance', 'metric', 'dashboarddata']
 )
